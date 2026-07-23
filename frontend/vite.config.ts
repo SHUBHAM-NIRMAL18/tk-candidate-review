52addea0d2bbb0e08c129b0139923d6c19f63b03
Shubham Nirmal <suvamniraml456@gmail.com>
2026-07-23 23:24:48 +0545

    chore(docker): configure frontend docker container, proxy target, and vite polling watch mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const target = process.env.VITE_BACKEND_URL || 'http://127.0.0.1:8000';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Expose server to all hosts (127.0.0.1 & localhost)
+    host: true,
     port: 5173,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: target,
         changeOrigin: true,
       },
     },
